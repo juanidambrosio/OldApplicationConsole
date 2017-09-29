85cb155a49d1f85b0315a25533e02b2a96fb7460
Juan Ignacio D'Ambrosio <dambrosiojuani@gmail.com>
2017-09-29 00:05:04 -0300

    Add files via upload

---
+++ OlderUnitTestsConsole/PaymentGateway.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace OlderUnitTestsConsole
+{
+    public interface IPaymentGateway
+    {
+         decimal GetTotal(List<string> items);
+    }
+
+    //Clase que uso para tests unitarios
+    public class MockGateway : IPaymentGateway
+    {
+        public decimal GetTotal(List<string> items)
+        {
+            return 0.0m;
+        }
+    }
+
+    //Clase que uso realmente para la lógica
+    public class PaymentGateway:IPaymentGateway
+    {
+
+
+        public decimal GetTotal(List<string> items)
+        {
+            //Autenticación
+            //Datos de la cuenta
+            //Procesar tarjeta de crédito
+            //Otras cosas que NO quiero hacer en un unit test...
+            return 5.5m;
+        }
+    }
+}
